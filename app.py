fe7f9f2885090498030cc5f769cb39137f35b057
Rish Ratnalingam <57958663+drxray73@users.noreply.github.com>
2023-02-22 14:38:40 +0000

    Added dynamic data and apis

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+CAFES = [
+  {
+    'id':1,
+    'name': 'Costa',
+    'location': 'Headingley'
+  },
+  {
+    'id':2,
+    'name': 'Starbucks',
+    'location': 'Moortown'
+  },
+{
+    'id':3,
+    'name': 'Nero',
+    'location': 'Alwoodley'
+  },
+  
+]
 
 
 @app.route("/")
 def hello_world():
-  return "Hello, World"
+  return render_template("index.html", cafes=CAFES)
 
+@app.route("/api/cafes")
+def list_cafes():
+  return jsonify(CAFES)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
